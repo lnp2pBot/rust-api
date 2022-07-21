36ca163e84a7bb5c0c7af47f14d4e64103cba0eb
Francisco Calderón <fjcalderon@gmail.com>
2022-07-21 18:34:05 -0300

    Add missing models

--- src/db/models.rs
+++ src/db/models.rs
@@ -42,3 +42,136 @@ pub struct Community {
     currencies: Vec<String>,
     created_at: DateTime,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    _id: bson::oid::ObjectId,
+    tg_id: String,
+    username: String,
+    lang: String,
+    trades_completed: f32,
+    total_reviews: f32,
+    last_rating: f32,
+    total_rating: f32,
+    volume_traded: f32,
+    admin: bool,
+    banned: bool,
+    show_username: bool,
+    show_volume_traded: bool,
+    lightning_address: Option<String>,
+    disputes: Option<f32>,
+    default_community_id: Option<String>,
+    created_at: DateTime,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum DisputeStatus {
+    #[serde(rename = "WAITING_FOR_SOLVER")]
+    WaitingForSolver,
+    #[serde(rename = "IN_PROGRESS")]
+    InProgress,
+    #[serde(rename = "SETTLED")]
+    Settled,
+    #[serde(rename = "SELLER_REFUNDED")]
+    SellerRefunded,
+    #[serde(rename = "RELEASED")]
+    Released,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Dispute {
+    initiator: String,
+    seller_id: Option<String>,
+    buyer_id: Option<String>,
+    status: DisputeStatus,
+    community_id: Option<String>,
+    order_id: String,
+    solver_id: Option<String>,
+    created_at: DateTime,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PendingPayment {
+    description: String,
+    amount: f32,
+    attempts: f32,
+    paid: bool,
+    is_invoice_expired: bool,
+    payment_request: String,
+    hash: Option<String>,
+    created_at: DateTime,
+    paid_at: Option<DateTime>,
+    user_id: String,
+    order_id: Option<String>,
+    community_id: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum OrderStatus {
+    #[serde(rename = "WAITING_PAYMENT")]
+    WaitingPayment,
+    #[serde(rename = "WAITING_BUYER_INVOICE")]
+    WaitingBuyerInvoice,
+    #[serde(rename = "PENDING")]
+    Pending,
+    #[serde(rename = "ACTIVE")]
+    Active,
+    #[serde(rename = "FIAT_SENT")]
+    FiatSent,
+    #[serde(rename = "CLOSED")]
+    Closed,
+    #[serde(rename = "DISPUTE")]
+    Dispute,
+    #[serde(rename = "CANCELED")]
+    Canceled,
+    #[serde(rename = "SUCCESS")]
+    Success,
+    #[serde(rename = "PAID_HOLD_INVOICE")]
+    PaidHoldInvoice,
+    #[serde(rename = "CANCELED_BY_ADMIN")]
+    CanceledByAdmin,
+    #[serde(rename = "EXPIRED")]
+    Expired,
+    #[serde(rename = "COMPLETED_BY_ADMIN")]
+    CompletedByAdmin,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Order {
+    description: String,
+    amount: f32,
+    fee: f32,
+    bot_fee: f32,
+    community_fee: f32,
+    routing_fee: f32,
+    hash: String,
+    secret: String,
+    creator_id: String,
+    seller_id: String,
+    buyer_id: String,
+    buyer_invoice: String,
+    buyer_dispute: bool,
+    seller_dispute: bool,
+    buyer_cooperativecancel: bool,
+    seller_cooperativecancel: bool,
+    canceled_by: Option<String>,
+    status: OrderStatus,
+    #[serde(rename = "type")]
+    kind: String,
+    fiat_amount: f32,
+    fiat_code: String,
+    payment_method: String,
+    created_at: DateTime,
+    invoice_held_at: Option<DateTime>,
+    taken_at: Option<DateTime>,
+    tg_chat_id: Option<String>,
+    tg_order_message: Option<String>,
+    tg_channel_message1: String,
+    range_parent_id: Option<String>,
+    price_from_api: bool,
+    price_margin: f32,
+    calculated: bool,
+    admin_warned: bool,
+    paid_hold_buyer_invoice_updated: bool,
+    community_id: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,40 @@ async fn rocket() -> _ {
     let x = chrono::Duration::weeks(1);
     println!("{x:?}");
     let db = db::connect::get_database().await;
-    let coll = db.collection::<db::models::Community>("communities");
-    let user = coll
+    let comm_coll = db.collection::<db::models::Community>("communities");
+    let comm = comm_coll
         .find_one(doc! {"name": "Tropykus P2P"}, None)
         .await
         .unwrap()
         .unwrap();
+    println!("{comm:?}");
+    let user_coll = db.collection::<db::models::User>("users");
+    let user = user_coll
+        .find_one(doc! {"username": "negrunch"}, None)
+        .await
+        .unwrap()
+        .unwrap();
     println!("{user:?}");
+    let dispute_coll = db.collection::<db::models::Dispute>("disputes");
+    let dispute = dispute_coll
+        .find_one(doc! {"status": "IN_PROGRESS"}, None)
+        .await
+        .unwrap()
+        .unwrap();
+    println!("{dispute:?}");
+    let pp_coll = db.collection::<db::models::PendingPayment>("pendingpayments");
+    let pp = pp_coll
+        .find_one(doc! {"description": "Retiro por admin @negrunch"}, None)
+        .await
+        .unwrap()
+        .unwrap();
+    println!("{pp:?}");
+    let order_coll = db.collection::<db::models::Order>("orders");
+    let order = order_coll
+        .find_one(doc! {"fiat_code": "ARS"}, None)
+        .await
+        .unwrap()
+        .unwrap();
+    println!("{order:?}");
     rocket::build().mount("/", routes![api_routes::index])
 }
