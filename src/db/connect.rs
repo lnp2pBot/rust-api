c0fa97afc27924fc386868a9d0f2e8817eed52ca
Francisco Calderón <fjcalderon@gmail.com>
2022-09-05 18:32:36 -0300

    Remove comments on db connect

--- src/db/connect.rs
+++ src/db/connect.rs
@@ -8,15 +8,7 @@ pub fn get_database() -> Database {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let database_name = env::var("DATABASE_NAME").expect("DATABASE_NAME must be set");
 
-    // Parse a connection string into an options struct.
-    // let mut client_options = ClientOptions::parse(database_url).await.expect("Error getting client options");
-
-    // Manually set an option.
-    // client_options.app_name = Some("lnp2pbot".to_string());
     let client = Client::with_uri_str(database_url).unwrap();
-
-    // Get a handle to the deployment.
-    // let client = Client::with_options(client_options).expect("Error getting db client");
 
     client.database(&database_name)
 }
